e71b3f96df45af84edac808fea7edac2e1129c64
imoshanghua <imoshanghua1314@outlook.com>
2022-09-04 02:38:42 +0800

    refactor: 友情链接统一使用链接模块管理

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,87 +1,87 @@
 export default {
     mode: 'universal',
-
+  
     head: {
-        title: 'Xiaomo Blog|等我熬尽一日苦，喂你一口甜!',
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' },
-            { name: 'format-detection', content: 'telephone=no' }
-        ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-            // 改用 highlight.js  layouts/page.vue
-            // { hid: 'prism', rel: 'stylesheet', href: '/css/prism.css' }
-        ],
-        script: [
-            // 彩色图标
-            { src: 'https://at.alicdn.com/t/font_556506_1n415osdgrl.js' },
-            // { src: '/js/prism.js' },
-            // 百度主推文章收录用
-            { src: 'https://zz.bdstatic.com/linksubmit/push.js' },
-            // 加入百度统计js，使用时自行添加为自己的
-            { src: 'https://hm.baidu.com/hm.js?e3a6702fe5553d80fe84804ab8207878' }
-        ]
+      title: 'Xiaomo Blog|等我熬尽一日苦，喂你一口甜!',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' },
+        { name: 'format-detection', content: 'telephone=no' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        // 改用 highlight.js  layouts/page.vue
+        // { hid: 'prism', rel: 'stylesheet', href: '/css/prism.css' }
+      ],
+      script: [
+        // 彩色图标
+        { src: 'https://at.alicdn.com/t/font_556506_1n415osdgrl.js' },
+        // { src: '/js/prism.js' },
+        // 百度主推文章收录用
+        { src: 'https://zz.bdstatic.com/linksubmit/push.js' },
+        // 加入百度统计js，使用时自行添加为自己的
+        { src: 'https://hm.baidu.com/hm.js?e3a6702fe5553d80fe84804ab8207878' }
+      ]
     },
-
+  
     loading: './components/Loading',
-
+  
     router: {
-        middleware: 'global'
+      middleware: 'global'
     },
-
+  
     css: [
-        'element-ui/lib/theme-chalk/index.css',
-        './assets/scss/index.scss'
+      'element-ui/lib/theme-chalk/index.css',
+      './assets/scss/index.scss'
     ],
-
+  
     styleResources: {
-        scss: ['./assets/scss/variable.scss']
+      scss: ['./assets/scss/variable.scss']
     },
-
+  
     plugins: [
-        '~/plugins/axios',
-        { src: '~/plugins/element-ui', ssr: true },
-        { src: '~/plugins/message', ssr: false },
-        { src: '~/plugins/icon', ssr: true }
-        // { src: '~/plugins/common', ssr: false }
+      '~/plugins/axios',
+      { src: '~/plugins/element-ui', ssr: true },
+      { src: '~/plugins/message', ssr: false },
+      { src: '~/plugins/icon', ssr: true }
+      // { src: '~/plugins/common', ssr: false }
     ],
-
+  
     modules: [
-        // Doc: https://axios.nuxtjs.org/usage
-        '@nuxtjs/axios',
-        '@nuxtjs/style-resources',
-        '@nuxtjs/proxy'
+      // Doc: https://axios.nuxtjs.org/usage
+      '@nuxtjs/axios',
+      '@nuxtjs/style-resources',
+      '@nuxtjs/proxy'
     ],
-
+  
     axios: {
-        proxy: true
+      proxy: true
     },
-
+  
     build: {
-        transpile: [/^element-ui/],
-        extractCSS: true,
-        vendors: ['@nuxtjs/axios', 'element-ui']
-            // extend(config, ctx) {
-            // }
+      transpile: [/^element-ui/],
+      extractCSS: true,
+      vendors: ['@nuxtjs/axios', 'element-ui']
+      // extend(config, ctx) {
+      // }
     },
-
+  
     // 将此处2个地址改为自己的地址
     proxy: {
-        '/api': {
-            // target: 'https://www.xuanmo.xin',
-            target: 'https://moshanghua.net',
-            pathRewrite: {
-                '^/api': '/'
-            }
-        },
-        '/wp-content': {
-            // target: 'https://www.xuanmo.xin'
-            target: 'https://moshanghua.net'
+      '/api': {
+        // target: 'https://www.xuanmo.xin',
+        target: 'https://moshanghua.net',
+        pathRewrite: {
+          '^/api': '/'
         }
+      },
+      '/wp-content': {
+        // target: 'https://www.xuanmo.xin'
+        target: 'https://moshanghua.net'
+      }
     },
-
+  
     env: {
-        baseUrl: '/api'
+      baseUrl: '/api'
     }
-}
+  }
